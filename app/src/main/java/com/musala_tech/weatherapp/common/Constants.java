2abb31e05f166e27f014fd7f06ddc1e554a57c83
Viktor Jovanovski <BuktopMKD@gmail.com>
2019-10-03 11:54:53 +0200

    Constants - Add API KEY

--- app/src/main/java/com/musala_tech/weatherapp/common/Constants.java
+++ app/src/main/java/com/musala_tech/weatherapp/common/Constants.java
@@ -1,4 +1,9 @@
 package com.musala_tech.weatherapp.common;
 
 public interface Constants {
+    /**
+     * https://openweathermap.org/api
+     * Account: BuktopMKD@gmail.com
+     */
+    String API_KEY = "06ccb8eddef7cbb14f57377c31607ffe";
 }
