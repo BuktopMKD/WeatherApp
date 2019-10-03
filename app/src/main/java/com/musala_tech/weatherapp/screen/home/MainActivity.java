b410fb0d7795215c310ce510233535cebdd53975
Viktor Jovanovski <BuktopMKD@gmail.com>
2019-10-03 13:10:28 +0200

    Create Package - Home

--- app/src/main/java/com/musala_tech/weatherapp/screen/home/MainActivity.java
+++ app/src/main/java/com/musala_tech/weatherapp/screen/home/MainActivity.java
@@ -1,12 +1,20 @@
-package com.musala_tech.weatherapp;
+package com.musala_tech.weatherapp.screen.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.musala_tech.weatherapp.R;
 
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static void start(Context context) {
+        context.startActivity(new Intent(context, MainActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
