944be6cbbaceb1779e8c8efd1bf5a7c5577269bb
Viktor Jovanovski <BuktopMKD@gmail.com>
2019-10-03 12:45:24 +0200

    Api Service - Add getCityWeather()

--- app/src/main/java/com/musala_tech/weatherapp/network/ApiService.java
+++ app/src/main/java/com/musala_tech/weatherapp/network/ApiService.java
@@ -1,4 +1,13 @@
 package com.musala_tech.weatherapp.network;
 
-public class ApiService {
+import com.musala_tech.weatherapp.model.WeatherResponse;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+
+    @GET("weather")
+    Single<WeatherResponse> getCityWeather(@Query("q") String city, @Query("APPID") String API_KEY);
 }
