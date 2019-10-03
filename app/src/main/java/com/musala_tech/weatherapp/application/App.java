857fbcad81877aba2415439e7c442b38d8ead695
Viktor Jovanovski <BuktopMKD@gmail.com>
2019-10-03 11:36:31 +0200

    App - Init LeakCanary, CrashLibrary, Timber

--- app/src/main/java/com/musala_tech/weatherapp/application/App.java
+++ app/src/main/java/com/musala_tech/weatherapp/application/App.java
@@ -1,8 +1,39 @@
+package com.musala_tech.weatherapp.application;
+
 import android.app.Application;
+
+import com.devs.acr.AutoErrorReporter;
+import com.musala_tech.weatherapp.BuildConfig;
+import com.squareup.leakcanary.LeakCanary;
+
+import timber.log.Timber;
+
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        initLeakCanary();
+        initCrashLibrary();
+        initTimber();
     }
+
+    private void initLeakCanary() {
+        LeakCanary.install(this);
     }
+
+    private void initCrashLibrary() {
+        if (BuildConfig.DEBUG) {
+            AutoErrorReporter.get(this)
+                    .setEmailAddresses("buktopmkd@gmail.com")
+                    .setEmailSubject("WeatherApp Crash Report")
+                    .start();
+        }
+    }
+
+    private void initTimber() {
+        Timber.plant(new Timber.DebugTree());
+    }
+
+
+}
